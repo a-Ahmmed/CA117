5f5fe7c19fa108f7831ce722cd82367c9c9d02d3
Aman Ahmmed <aman.ahmmed2@mail.dcu.ie>
2023-02-01 10:37:49 +0000

    Solutions for Week 3 tasks

---
+++ week-03/ListComprehension/primecomps_031.py
@@ -0,0 +1,10 @@
+import sys
+
+s = sys.stdin.readline()
+while s != "":
+    s = int(s.strip()) + 1
+
+    num = [n for n in range(8,s) if n % 2 != 0 and n % 3 != 0 and n % 5 != 0 and n % 7 != 0]
+    print(f'Primes: {[2,3,5,7] + num}')
+
+    s = sys.stdin.readline()
